2933780389455e6a276e7883247b0a6666f3ef20
noxiousghost <sapkota.king@gmail.com>
2024-12-04 09:56:59 +0545

    code for day 1

---
+++ 2024/day 1/day1.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+const readline = require("readline");
+const input = fs.createReadStream("./input.txt", "utf-8");
+
+const rl = readline.createInterface({
+  input: input,
+});
+
+rl.on("line", (line) => {
+  const [value1, value2] = line.trim().split(/\s+/);
+  arrLeft.push(parseInt(value1, 10));
+  arrRight.push(parseInt(value2, 10));
+  return;
+});
+
+const arrLeft = [];
+const arrRight = [];
+
+async function partOne(arrLeft, arrRight) {
+  arrLeft.sort((a, b) => {
+    return a - b;
+  });
+  arrRight.sort((a, b) => {
+    return a - b;
+  });
+  let totalCount = 0;
+  for (let i = 0; i < arrLeft.length; i++) {
+    totalCount += Math.abs(arrLeft[i] - arrRight[i]);
+  }
+  console.log("Part One: ", totalCount);
+}
+
+async function partTwo(arrLeft, arrRight) {
+  let totalCount = 0;
+  arrLeft.forEach((item) => {
+    const numOfOccurrences = arrRight.filter((x) => x === item).length;
+    totalCount += item * numOfOccurrences;
+  });
+
+  console.log("Part Two: ", totalCount);
+}
+
+rl.on("close", async () => {
+  await partOne(arrLeft, arrRight);
+  await partTwo(arrLeft, arrRight);
+});
